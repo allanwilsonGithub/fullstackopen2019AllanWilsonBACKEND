9796269c394f3a3165aaa4ab06ea253b16236d06
Allan Wilson <allan@allanwilson.net>
2020-01-16 00:20:08 +0200

    4.2 Blog list, step2 DONE

--- index.js
+++ index.js
@@ -3,72 +3,29 @@ const app = express()
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const cors = require('cors')
-const Person = require('./models/personDb')
+const Blog = require('./models/bloglistDb')
 
 app.use(cors())
 app.use(express.static('build'))
 app.use(bodyParser.json())
 app.use(morgan('tiny'))
 
-
-
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
-})
-
-app.get('/info', (request, response) => {
-  Person.find({}).then(person => {response.send(`
-  <p>Phonebook has info for ${person.length} people</p>
-  <p>${Date()}</p>
-  `)
-  })})
-
-app.get('/api/persons/', (request, response) => {
-  Person.find({}).then(person => {
-    response.json(person)
-  })
-})
-
-app.get('/api/persons/:id', (request, response, next) => {
-  Person.findById(request.params.id)
-    .then(person => {
-      if (person) {
-        response.json(person)
-      } else {
-        response.status(404).end()
-      }
+app.get('/api/blogs', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
     })
-    .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  Person.findById(request.params.id).then(person => {
-    person.deleteOne()
-  }).then(response.status(204).end())
-})
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
 
-app.post('/api/persons', (request, response, next) => {
-  const body = request.body
-
-  if (!body.name) {
-    return response.status(400).json({
-      error: 'name missing'
-    })}
-  else if (!body.number) {
-    return response.status(400).json({
-      error: 'number missing'
-    })}
-
-  const person = new Person ({
-    content: body.content,
-    name: body.name || false,
-    number: body.number || false
-  })
-
-  person.save().then(savedPerson => {
-    response.json(savedPerson.toJSON())
-  })
-    .catch(error => next(error))
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
 })
 
 const errorHandler = (error, request, response, next) => {
@@ -84,7 +41,7 @@ const errorHandler = (error, request, response, next) => {
 
 app.use(errorHandler)
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3003
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
--- models/bloglistDb.js
+++ models/bloglistDb.js
@@ -14,14 +14,16 @@ mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
     console.log('error connecting to MongoDB:', error.message)
   })
 
-const personSchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true },
-  number: Number
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
 })
 
-personSchema.plugin(uniqueValidator, { type: 'mongoose-unique-validator' })
+blogSchema.plugin(uniqueValidator, { type: 'mongoose-unique-validator' })
 
-personSchema.set('toJSON', {
+blogSchema.set('toJSON', {
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString()
     delete returnedObject._id
@@ -29,4 +31,4 @@ personSchema.set('toJSON', {
   }
 })
 
-module.exports = mongoose.model('Person', personSchema)
+module.exports = mongoose.model('Blog', blogSchema)
