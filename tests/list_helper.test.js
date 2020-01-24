78dcfae046374d35df86793d626066979c33b04a
Allan Wilson <allan@allanwilson.net>
2020-01-25 01:25:09 +0200

    4.5*: helper functions and unit tests, step3 DONE

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -51,3 +51,68 @@ test('of larger list returns correct sum of likes', () => {
   expect(result).toBe(21)
 })
 })
+
+describe('favourite blog', () => {
+
+test('of multiple blogs', () => {
+  const blogs = [
+    {
+      _id: '5a422aa71b54a676234d17f7',
+      title: "Burns Night 1",
+      author: "Robert Burns a",
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: "Burns Night 2",
+      author: "Robert Burns b",
+      likes: 7,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b54a676234d17f9',
+      title: "Burns Night 3",
+      author: "Robert Burns c",
+      likes: 9,
+      __v: 0
+    }]
+
+  const result = listHelper.favoriteBlog(blogs)
+  expect(result).toEqual(
+      {
+      title: "Burns Night 3",
+      author: "Robert Burns c",
+      likes: 9
+    }
+  )
+})
+
+test('of empty array', () => {
+  const blogs = []
+
+  const result = listHelper.favoriteBlog(blogs)
+  expect(result).toBe("Not enough blogs to compare")
+})
+})
+
+test('of single blog', () => {
+  const blogs = [
+    {
+      _id: '5a422aa71b54a676234d17f9',
+      title: "Burns Night 3",
+      author: "Robert Burns c",
+      likes: 9,
+      __v: 0
+    }
+  ]
+
+  const result = listHelper.favoriteBlog(blogs)
+    expect(result).toEqual(
+      {
+      title: "Burns Night 3",
+      author: "Robert Burns c",
+      likes: 9
+    }
+  )
+})
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -3,10 +3,25 @@ const dummy = (blogs) => {
 }
 
 const totalLikes = (blogs) => {
-  const likesArray = blogs.map(x => x.likes);
+  const likesArray = blogs.map(x => x.likes)
   return likesArray.reduce((a, b) => a + b, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length > 0) {
+    const likesArray = blogs.map(x => x.likes)
+    const mostLikedBlogLikes = Math.max( ...likesArray )
+    const mostLikedBlog = blogs.filter(x => x.likes === mostLikedBlogLikes)
+    newArray = [
+      {'title': mostLikedBlog[0].title,
+       'author': mostLikedBlog[0].author,
+       'likes': mostLikedBlog[0].likes
+      }]
+    return newArray[0]
+  } else {
+    return "Not enough blogs to compare"
+  }}
+
 module.exports = {
-  dummy, totalLikes
+  dummy, totalLikes, favoriteBlog
 }
