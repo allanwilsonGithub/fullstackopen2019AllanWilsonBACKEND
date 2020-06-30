79232bcd6c6f4abfef537faa961773ac1b3544c5
Allan Wilson <allan@allanwilson.net>
2020-07-01 01:53:07 +0300

    ESLint update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,8 @@
 module.exports = {
   'env': {
-    'browser': true,
+    'es6': true,
     'commonjs': true,
-    'es2020': true
+    'node': true
   },
   'extends': 'eslint:recommended',
   'parserOptions': {
