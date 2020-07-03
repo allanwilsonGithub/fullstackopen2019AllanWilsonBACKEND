b3cfbe43ba4bae6f4acefbd44e84fbad73993a09
Allan Wilson <allan@allanwilson.net>
2020-07-03 21:24:52 +0300

    4.4: helper functions and unit tests, step2 DONE

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,19 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  const blogsLikesArray = blogs.map((blog, i) => blog.likes)
+
+const totalNumberOfLikes = blogsLikesArray.reduce(
+(acc, curr, i)=>acc+curr,
+0)
+return totalNumberOfLikes
+}
+
+
+
+
+
 module.exports = {
-  dummy
+  dummy, totalLikes
 }
