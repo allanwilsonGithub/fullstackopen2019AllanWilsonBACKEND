3352d17987fdd135cc8b446eb1da30e3487e2507
Allan Wilson <allan@allanwilson.net>
2020-07-04 00:25:01 +0300

    4.5*: helper functions and unit tests, step3 DONE

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,20 +1,24 @@
 const dummy = (blogs) => {
+  console.log(blogs)
   return 1
 }
 
 const totalLikes = (blogs) => {
-  const blogsLikesArray = blogs.map((blog, i) => blog.likes)
+  const blogsLikesArray = blogs.map(blog => blog.likes)
 
-const totalNumberOfLikes = blogsLikesArray.reduce(
-(acc, curr, i)=>acc+curr,
-0)
-return totalNumberOfLikes
+  const totalNumberOfLikes = blogsLikesArray.reduce(
+    (acc, curr) => acc+curr,
+    0)
+  return totalNumberOfLikes
 }
 
-
-
-
+const favoriteBlog = (blogs) => {
+  const blogsLikesArray = blogs.map(blog => blog.likes)
+  const maxLikes = Math.max(...blogsLikesArray)
+  const faveBlog = blogs.find(blog => blog.likes === maxLikes)
+  return { 'author': faveBlog.author, 'likes': faveBlog.likes, 'title': faveBlog.title }
+}
 
 module.exports = {
-  dummy, totalLikes
+  dummy, totalLikes, favoriteBlog
 }
